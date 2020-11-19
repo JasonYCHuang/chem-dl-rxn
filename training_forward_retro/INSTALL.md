204dd9175c600bc4f170109f4377e934c36a0aec
JasonYCHuang <jasonych99@gmail.com>
2020-11-19 20:27:14 +0100

    add training for forward and retro

---
+++ training_forward_retro/INSTALL.md
@@ -0,0 +1,26 @@
+# INSTALL
+
+
+```bash
+$ conda create --name retro-forward python=3.6
+$ source activate retro-forward
+```
+
+```bash
+$ conda install -c rdkit rdkit=2018.03.4.0
+$ sudo apt-get install gcc libxrender1 libxext-dev
+$ conda install pytorch torchvision cpuonly -c pytorch
+```
+
+```bash
+$ git clone https://github.com/pytorch/fairseq
+$ cd fairseq
+$ pip install --editable ./
+$ cd ..
+```
+
+```bash
+$ git clone XXX
+$ cd XXX
+$ pip install -r requirements.txt
+```
---
+++ training_forward_retro/preprocess/prepare-rxn.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# Adapted from https://github.com/facebookresearch/MIXER/blob/master/prepareData.sh
+
+MOSES_GIT=https://github.com/moses-smt/mosesdecoder.git
+if [ -d 'mosesdecoder' ]; then
+    echo "Mosesdecoder has already been cloned."
+else
+    echo 'Cloning Moses github repository (for tokenization scripts)...'
+    git clone $MOSES_GIT
+fi
+
+SUBWORD_GIT=https://github.com/rsennrich/subword-nmt.git
+if [ -d 'subword-nmt' ]; then
+    echo "Subword-nmt has already been cloned."
+else
+    echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+    git clone $SUBWORD_GIT
+fi
+
+
+
+BPEROOT=subword-nmt/subword_nmt
+BPE_TOKENS=600
+
+GZ=download_small
+prep=uspto
+tmp=$prep/tmp
+orig=orig
+
+src=rct
+tgt=prd
+# condition=MIT_mixed
+
+if [ -d 'orig' ]; then
+    echo "Data has already been downloaded."
+else
+    mkdir -p $orig $tmp $prep
+    echo "Downloading data from ${URL}..."
+    cd $orig
+    cp '../../backup/download_small' .
+
+    if [ -f $GZ ]; then
+        echo "Data successfully downloaded."
+    else
+        echo "Data not successfully downloaded."
+        exit
+    fi
+    tar zxvf $GZ
+    cd ..
+fi
+
+TRAIN=$tmp/train.rct-prd
+BPE_CODE=$prep/code
+rm -f $TRAIN
+for l in $src $tgt; do
+    cat $orig/$prep/$condition/train.$l >> $TRAIN
+done
+
+echo "learn_bpe.py on ${TRAIN}..."
+python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
+
+for L in $src $tgt; do
+    for f in train.$L val.$L test.$L; do
+        echo "apply_bpe.py to ${f}..."
+        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $orig/$prep/$condition/$f > $prep/$f
+    done
+done
+
+echo "BPE_TOKENS=${BPE_TOKENS}"
+
+echo "DONE!..."
+
+
